e4c489a110ae6bf41b50d8f3d9b3f1bf697428f4
anonymoushackingtool <74699731+anonymoushackingtool@users.noreply.github.com>
2021-01-07 15:49:05 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"Currently Only supports Youtube Single  (No playlist) Just Send Youtube Url"
+    helptxt = f"Currently only supports Youtube Single  videos(No playlist),Just Send Youtube Url.\n\nLike:http://www.youtube.com/watch?v=6Ojs-T8936g\n\nYou can also use @vid or @youtube in inline mode to search youtube videos.\n\nExample: Type \"@vid silenceandroidgamer\"\nOr\nType \"@youtube silenceandroidgamer\"."
     await message.reply_text(helptxt)
