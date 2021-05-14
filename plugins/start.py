df21e68217e4ef093aaf4e0876980b8668b8fde6
Anonymoushackingtool <74699731+anonymoushackingtool@users.noreply.github.com>
2021-05-14 19:11:24 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,4 +1,5 @@
 from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 
 @Client.on_message(Filters.command(["start"]), group=-2)
 async def start(client, message):
@@ -12,3 +13,15 @@ async def start(client, message):
     welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
+    
+    @app.on_message(filters.text & filters.private )
+def echo(client, message):
+ update_channel = "TG_Free_Bots"
+ user_id = message.from_user.id
+ if update_channel :
+  try:
+   client.get_chat_member(update_channel, user_id)
+  except UserNotParticipant:
+   message.reply_text("**__Join our Bot's Channel to use ME__** ",parse_mode="markdown", reply_to_message_id = message.message_id, reply_markup = InlineKeyboardMarkup([ [ InlineKeyboardButton("Join our Bot's channel" ,url="https://t.me/TG_Free_Bots") ]
+   ]))
+   return
