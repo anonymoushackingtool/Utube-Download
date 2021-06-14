c9014665be740dfd533adf88eb3b489982b6283b
Anonymoushackingtool <74699731+anonymoushackingtool@users.noreply.github.com>
2021-06-14 11:29:03 +0530

    Update youtube_callback_data.py

--- plugins/youtube_callback_data.py
+++ plugins/youtube_callback_data.py
@@ -129,6 +129,7 @@ async def catch_youtube_dldata(c, q):
             media=filename,
             thumb=thumb_image_path,
             caption=os.path.basename(f" **{filename}** \n\nDᴏᴡɴʟᴏᴀᴅᴇᴅ ʙʏ @Utube_Downloadbot\n𝐎𝐭𝐡𝐞𝐫 𝐔𝐬𝐞𝐟𝐮𝐥 𝐅𝐫𝐞𝐞 𝐁𝐨𝐭𝐬:@TG_Free_Bots"),
+            print("hi")
         )
 
     if cb_data.startswith("docvideo"):
