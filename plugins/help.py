a957929264b9b17273f405ea81781971d9472b11
Anonymoushackingtool <74699731+anonymoushackingtool@users.noreply.github.com>
2021-06-15 15:37:27 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,7 +3,7 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"Currently only supports Youtube Single  videos(No playlist),Just Send Youtube Url.\n\nLike:http://www.youtube.com/watch?v=6Ojs-T8936g\n\nYou can also use @vid or @youtube in inline mode to search youtube videos.\n\nExample: Type \"```@vid silenceandroidgamer```\"\nOr\nType \"```@youtube silenceandroidgamer```\"."
+    helptxt = f"Currently only supports Youtube Single  videos(No playlist),Just Send Youtube Url.\n\nlike:http://www.youtube.com/watch?v=6Ojs-T8936g\nOr\nhttps://youtu.be/6Ojs-T8936g\n\nYou can also use ```@vid``` in inline mode to search youtube videos.\nExample: Type \" ```@vid silenceandroidgamer``` \"."
     await message.reply_text(
     text = helptxt,
     disable_web_page_preview=True)
